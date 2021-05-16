ac7c3ecc96f56797e43905d5748dc12a7b7212f6
James Earl Douglas <james@earldouglas.com>
2021-05-16 09:10:29 -0700

    Add comments for why each instance is nonlinear

--- example/src/main/scala/Example.scala
+++ example/src/main/scala/Example.scala
@@ -5,7 +5,7 @@ import com.earldouglas.linearscala.Linear
 case class Box(value: Int) extends Linear
 
 trait UnusedField {
-  val box: Box = Box(42)
+  val box: Box = Box(42) // box is never used
 }
 
 trait FieldUsedOnce {
@@ -15,23 +15,23 @@ trait FieldUsedOnce {
 
 trait FieldUsedTwice {
   val box: Box = Box(42)
-  println(box)
-  println(box)
+  println(box) // box is used twice
+  println(box) // box is used twice
 }
 
 trait UnusedParameter {
-  def foo(x: Box, y: Box): Int =
+  def foo(x: Box, y: Box): Int = // y is never used
     x.value
 }
 
 trait UnusedMethod {
-  def foo(): Box = Box(42)
+  def foo(): Box = Box(42) // foo is never used
 }
 
 trait UnusedValue {
   def foo(): Unit = {
     val x: Box = Box(42)
-    println(x)
-    println(x)
+    println(x) // x is used twice
+    println(x) // x is used twice
   }
 }
