2cb91232c9f6e8e0bd00443418f75f027161a049
James Earl Douglas <james@earldouglas.com>
2021-05-16 09:16:56 -0700

    Add some comments

--- rules/src/main/scala/fix/LinearTypes.scala
+++ rules/src/main/scala/fix/LinearTypes.scala
@@ -22,6 +22,7 @@ final class LinearTypes extends SemanticRule("LinearTypes") {
   override val isLinter: Boolean = true
   override val isRewrite: Boolean = false
 
+  // Try to figure out if the symbol represents a value that extends Linear
   private def isALinear(symbol: Symbol)(implicit doc: SemanticDocument): Boolean =
     symbol.info match {
       case Some(info) =>
@@ -45,12 +46,14 @@ final class LinearTypes extends SemanticRule("LinearTypes") {
       case None => false
     }
 
+  // Find all the terms that dereference a Linear value
   private def findDerefs(implicit doc: SemanticDocument): Map[Symbol, List[Term]] =
     doc.tree.collect {
       case t @ Term.Name(name) if t.isReference && isALinear(t.symbol) => t
     }
     .groupBy(_.symbol)
 
+  // Find all the terms that are references to a Linear value
   private def findRefs(implicit doc: SemanticDocument): Iterable[Term] =
     doc.tree.collect {
       case t @ Term.Name(name) if !t.isReference && isALinear(t.symbol) => t
@@ -61,17 +64,20 @@ final class LinearTypes extends SemanticRule("LinearTypes") {
     val derefs: Map[Symbol, List[Term]] = findDerefs(doc)
     val refs: Iterable[Term] = findRefs(doc)
 
+    // Find all Linear values that are used multiple times
     val overused: Iterable[Diagnostic] =
       derefs
         .filter(_._2.length > 1)
         .values
         .flatMap(ts => ts.map(t => MultipleDerefs(t, ts.length)))
 
+    // Find all Linear values that are never used
     val underused: Iterable[Diagnostic] =
      refs
        .filterNot(t => derefs.keySet.contains(t.symbol))
        .map(t => ZeroDerefs(t))
 
+    // Convert the above diagnostics into a Patch
     (overused ++ underused)
       .map(Patch.lint)
       .asPatch
