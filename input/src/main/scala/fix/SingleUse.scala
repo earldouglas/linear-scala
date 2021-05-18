7bc385b6156c73db165d33f75c99b6a1663db277
James Earl Douglas <james@earldouglas.com>
2021-05-18 05:45:07 -0700

    Use the tests in input/ as documentation (#10)
    This deprecates the project in *example/* as a source of documentation,
    in favor of the readme and the tests in *input/*.
    
    * Drop the redundant and unused project in *example/*
    * Add project setup documentation to the readme
    * Adds comments targeted at users to each of the tests in *input/*

--- input/src/main/scala/fix/SingleUse.scala
+++ input/src/main/scala/fix/SingleUse.scala
@@ -3,6 +3,9 @@ rule = LinearTypes
 */
 package fix
 
+/**
+ * Allow a [[Box]] field to be dereferenced exactly once.
+ */
 trait FieldUsedOnce {
   val box: Box = Box(42)
   println(box)
--- input/src/main/scala/fix/Unused.scala
+++ input/src/main/scala/fix/Unused.scala
@@ -3,29 +3,43 @@ rule = LinearTypes
 */
 package fix
 
+/**
+ * Don't allow a [[Box]] field to be created but never dereferenced.
+ */
 trait UnusedField {
   val box: Box = // assert: LinearTypes
     Box(42)
 }
 
+/**
+ * Don't allow a [[Box]] parameter to be declared but never dereferenced.
+ */
 trait UnusedParameter {
   def foo(x: Box, y: Box): Int = // assert: LinearTypes
     x.value
 }
 
+/**
+ * Don't allow a [[Box]] method to be created but never dereferenced.
+ */
 trait UnusedMethod {
   def foo(): Box = // assert: LinearTypes
     Box(42)
 }
 
+/**
+ * Don't allow a [[Box]] value to be created but never dereferenced.
+ */
 trait UnusedValue {
   def foo(): Unit = {
-    val x: Box = Box(42)
-    println(x) // assert: LinearTypes
-    println(x) // assert: LinearTypes
+    val x: Box = Box(42) // assert: LinearTypes
   }
 }
 
+/**
+ * Don't allow a [[Box]] binding in a for comprehension to be
+ * created but never dereferenced.
+ */
 trait UnusedBinding {
   for {
     x <- Some(Box(6)) // assert: LinearTypes
