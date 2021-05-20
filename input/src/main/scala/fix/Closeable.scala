ef7895f334f0110544d9ba7be220d293bc762aed
James Earl Douglas <james@earldouglas.com>
2021-05-20 05:21:29 -0700

    Add an example of a single-use Closeable (#12)
    This adds an example of file handle that can't accidentally be read
    after it has been closed.

---
+++ input/src/main/scala/fix/Closeable.scala
@@ -0,0 +1,29 @@
+/*
+rule = LinearTypes
+*/
+package fix
+
+import com.earldouglas.linearscala.Linear
+import java.io.RandomAccessFile
+
+/**
+ * Don't allow a [[Linear]] file to be dereferenced more than once,
+ * because it might already be closed.
+ */
+trait ReadFromClosedCloseable {
+
+  def readLineAndCloseFile(
+    f: RandomAccessFile
+  ): String = {
+    val line = f.readLine()
+    f.close()
+    line
+  }
+
+  val f: RandomAccessFile with Linear =
+    new RandomAccessFile("/etc/passwd", "r")
+      .asInstanceOf[RandomAccessFile with Linear]
+
+  readLineAndCloseFile(f) // assert: LinearTypes
+  f.readLine() // assert: LinearTypes
+}
